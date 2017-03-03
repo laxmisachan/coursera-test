b67d160c44c8480f9d6cab1d5d4a927e556cb9d0
Laxmi <laxmi@coursera.com>
2017-03-03 07:39:59 +0530

    module2 sulution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,28 +6,34 @@ angular.module("LunchCheck",[])
 LunchCheckController.$inject= ['$scope'];
 function LunchCheckController($scope){
   $scope.foodname ="";
-  $scope.msg ="";
 
-  $scope.calculateItems= function(){
-    $scope.msg="Enjoy";
-    $scope.fontColor ="";
-    var foodArray = $scope.foodname.split(",");
+  $scope.calculateItems= function(foodname){
+    const separator =',';
+    var foodArray = foodname.split(separator);
+    $scope.msg="";
+    $scope.count=0;
+    console.log(foodArray);
 
-   if ( $scope.foodname.length == 0 )
-    {
+
+  if ( foodArray == "" )
        $scope.msg="First enter data !";
-       $scope.fontColor = "red";
-    }
-    else {
-      var no = foodArray.length;
-      $scope.fontColor = "green";
-      if ( no <= 3)
+  else
+  {
+    for( var i=0;i<foodArray.length;i++)
       {
-         $scope.msg="Enjoy"; }
-         else {
-           $scope.msg="Too much";
-         }
+      console.log(foodArray[i]);
+      if ( foodArray[i] === ' ' || foodArray[i]=== ''){
+        console.log(foodArray[i]);
+        continue;
       }
-     }
+      $scope.count++;
+    }
+    console.log($scope.count);
+    if ($scope.count <= 3)
+       $scope.msg="Enjoy";
+    else
+        $scope.msg="Too much !";
+   }
+  }
 };
 })();
---
+++ module2-solution/README.txt
@@ -0,0 +1,1 @@
+Solution for module2 assignment.
